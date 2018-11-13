90d68e2be93de04abdb11c9d1f885d7a641f92a0
ewen <ewen.grosjean@free.fr>
2018-11-13 23:45:01 +0100

    strette-alist-get + documentation + fix moving the point of invisible window/buffer

--- strette.el
+++ strette.el
@@ -92,10 +92,14 @@
      (setq ,logs (cdr ,logs))))
 
 (defun strette--count-characters (f-name)
-  (let ((wc-string (shell-command-to-string (format "wc -c %s" f-name))))
-    (save-match-data
-      (string-match "\\([0-9]+\\)" wc-string)
-      (string-to-number (match-string-no-properties 1 wc-string)))))
+  (with-temp-buffer
+    (let ((status (call-process "wc" nil t nil "-c" f-name)))
+      (if (equal status 0)
+          (let ((wc-string (buffer-substring-no-properties (point-min) (point-max))))
+            (save-match-data
+              (string-match "\\([0-9]+\\)" wc-string)
+              (string-to-number (match-string-no-properties 1 wc-string))))
+        (error (buffer-substring-no-properties (point-min) (point-max)))))))
 
 (defun strette--make-log (log-keys match-list message)
   (let ((l (cddr match-list))
@@ -148,7 +152,14 @@
       (delete-region (point-min) match-search-start-point)
       logs)))
 
+(defun strette-alist-get (alist k &optional default)
+  "Return the value associated with KEY in ALIST. If KEY is not found in ALIST, return DEFAULT. keys are compared using `equal`."
+  (cond ((null alist) default)
+        ((equal (caar alist) k) (cdar alist))
+        (t (strette-alist-get (cdr alist) k default))))
+
 (defun strette-alist-set (alist k v)
+  "Non destructively associate the value V with- the key K in the alist ALIST. If ALIST already contains K, then its value (the first one if there are multiple K) is changed to V. If ALIST does not contain a key K, they the pair (K . V) is cons-ed to ALIST. Keys are compared using `equal`"
   (cond ((null alist) `((,k . ,v)))
         ((equal (caar alist) k) (cons `(,k . ,v) (cdr alist)))
         (t (cons (car alist) (strette-alist-set (cdr alist) k v)))))
@@ -203,13 +214,16 @@
         (restore-buffer-modified-p nil)))))
 
 (defmacro strette--save-excursion-or-move-forward (&rest body)
-  `(progn
-     (if (equal (point) (point-max))
-         (progn
-           ,@body
-           (set-window-point (get-buffer-window) (point-max)))
-       (save-excursion
-         ,@body))))
+  (let ((result-sym (make-symbol "result")))
+    `(progn
+       (if (equal (point) (point-max))
+           (let ((,result-sym (progn ,@body)))
+             (when-let (w (get-buffer-window nil t))
+               (goto-char (point-max))
+               (set-window-point w (point-max)))
+             ,result-sym)
+         (save-excursion
+           ,@body)))))
 
 (defun strette--append-message (buffer logs log-formatter log-filter message-tail)
   (when-let (log (strette--logs-first logs))
@@ -344,6 +358,27 @@
 
 (defun strette-start (f-name buffer-name log-regexp log-keys log-formatter log-filter
                              &optional logs-limit)
+  "Watch the log file F-NAME and display its content in a buffer named BUFFER-NAME.
+
+If BUFFER-NAME already exists and is already a strette buffer, then it is cleared and reused. If BUFFER-NAME already exists and is not a strette buffer, an error is thrown. If BUFFER-NAME does not exist, it is created.
+
+Each line of the F-NAME file is matched against the LOG-REGEXP regexp and reified into an association list with the following format:
+`((key1 . match-group1)
+  (key2 . match-group2)
+  ...
+  (keyN . match-groupN)
+  (:message . message-text))
+where the keys are the elements of the LOG-KEYS list and the match-groups are the groups defined in the LOG-REGEXP. The reified log always contains a :message entry which value is the text that didn't match the regexp, including the lines found between two regexp match.
+
+LOG-FORMATTER is a function which only argument is a log and must return a string which is the serialized representation of the log.
+
+LOG-FILTER is a function which only argument is a log. LOG-FILTER must return a log which will be output into the BUFFER-NAME buffer. LOG-FILTER can return nil to exclude a log from the output buffer. LOG-FILTER is allowed to modify the values associated with the log keys.
+
+LOGS-LIMIT is an optional parameter representing the maximum number of logs written to the output buffer. Its default value is 100. Passing nil as a LOGS-LIMIT will limit the number of logs to 100. The LOGS-LIMIT can be disabled by passing the value :no-limit.
+
+Additional consideration:
+ - Every log entry in the F-NAME log file is expected to start after a newline character.
+ - The LOG-FILTER can be called multiple times with a same log parameter and as such, must be free of side effects. The LOG-FILTER function can be called with a log parameter which :message value is truncated but the LOG-FILTER is guaranteed to be called at least once with its full :message value. Strette will take care of writing a same log only once in the output buffer."
   (add-hook 'kill-buffer-hook 'strette--kill-buffer-hook)
   (let* ((f-name (expand-file-name f-name))
          (logs-limit (if (equal logs-limit :no-limit)
@@ -434,6 +469,7 @@
    (strette-start f-name "*test.log*" log-regexp log-keys log-formatter log-filter))
 
  (list-processes)
+
  )
 
 (provide 'strette)
@@ -443,3 +479,5 @@
 ;; (not really what we want)
 
 ;; We expect every log message to start after a newline character
+
+;; strette--save-excursion-or-move-forward - invisible frames
