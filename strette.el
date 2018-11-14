1983ec7e33e747569f8b119c759d33a1e07776d2
ewen <ewen.grosjean@free.fr>
2018-11-14 23:07:21 +0100

    comments

--- strette.el
+++ strette.el
@@ -159,7 +159,7 @@
         (t (strette-alist-get (cdr alist) k default))))
 
 (defun strette-alist-set (alist k v)
-  "Non destructively associate the value V with- the key K in the alist ALIST. If ALIST already contains K, then its value (the first one if there are multiple K) is changed to V. If ALIST does not contain a key K, they the pair (K . V) is cons-ed to ALIST. Keys are compared using `equal`"
+  "Non destructively associate the value V with the key K in the alist ALIST. If ALIST already contains K, then its value (the first one if there are multiple K) is changed to V. If ALIST does not contain a key K, then the pair (K . V) is cons-ed to ALIST. Keys are compared using `equal`."
   (cond ((null alist) `((,k . ,v)))
         ((equal (caar alist) k) (cons `(,k . ,v) (cdr alist)))
         (t (cons (car alist) (strette-alist-set (cdr alist) k v)))))
@@ -218,8 +218,8 @@
     `(progn
        (if (equal (point) (point-max))
            (let ((,result-sym (progn ,@body)))
+             (goto-char (point-max))
              (when-let (w (get-buffer-window nil t))
-               (goto-char (point-max))
                (set-window-point w (point-max)))
              ,result-sym)
          (save-excursion
@@ -362,7 +362,7 @@
 
 If BUFFER-NAME already exists and is already a strette buffer, then it is cleared and reused. If BUFFER-NAME already exists and is not a strette buffer, an error is thrown. If BUFFER-NAME does not exist, it is created.
 
-Each line of the F-NAME file is matched against the LOG-REGEXP regexp and reified into an association list with the following format:
+Each line of the F-NAME file is matched against the LOG-REGEXP regexp and matching lines are reified into an association list with the following format:
 `((key1 . match-group1)
   (key2 . match-group2)
   ...
@@ -370,13 +370,13 @@ Each line of the F-NAME file is matched against the LOG-REGEXP regexp and reifie
   (:message . message-text))
 where the keys are the elements of the LOG-KEYS list and the match-groups are the groups defined in the LOG-REGEXP. The reified log always contains a :message entry which value is the text that didn't match the regexp, including the lines found between two regexp match.
 
-LOG-FORMATTER is a function which only argument is a log and must return a string which is the serialized representation of the log.
+LOG-FORMATTER is a function which only argument is a log. LOG-FORMATTER must return a string which is the serialized representation of the log.
 
 LOG-FILTER is a function which only argument is a log. LOG-FILTER must return a log which will be output into the BUFFER-NAME buffer. LOG-FILTER can return nil to exclude a log from the output buffer. LOG-FILTER is allowed to modify the values associated with the log keys.
 
 LOGS-LIMIT is an optional parameter representing the maximum number of logs written to the output buffer. Its default value is 100. Passing nil as a LOGS-LIMIT will limit the number of logs to 100. The LOGS-LIMIT can be disabled by passing the value :no-limit.
 
-Additional consideration:
+Additional considerations:
  - Every log entry in the F-NAME log file is expected to start after a newline character.
  - The LOG-FILTER can be called multiple times with a same log parameter and as such, must be free of side effects. The LOG-FILTER function can be called with a log parameter which :message value is truncated but the LOG-FILTER is guaranteed to be called at least once with its full :message value. Strette will take care of writing a same log only once in the output buffer."
   (add-hook 'kill-buffer-hook 'strette--kill-buffer-hook)
@@ -481,3 +481,5 @@ Additional consideration:
 ;; We expect every log message to start after a newline character
 
 ;; strette--save-excursion-or-move-forward - invisible frames
+
+;; byte-recompile to check warnings ----  C-u 0 M-x byte-recompile-directory
